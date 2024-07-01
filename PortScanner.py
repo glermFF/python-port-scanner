36a8c2af58469cf43add88234bab443d1d4ce796
Guilherme Ferreira Freitas <87777795+glermFF@users.noreply.github.com>
2024-07-01 19:19:25 -0300

    "lil updates"

--- PortScanner.py
+++ PortScanner.py
@@ -2,15 +2,30 @@
 
 import socket
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = [80, 433, 8080, 9090]
 
-def scanPort(port):
-    if soc.connect_ex((host, port)):
-        print(f"Port {port} status: Open")
-    else:
-        print(f"Port {port} status: Closed")
+def scanPort(host):
+    
+    for i in port:
+        if tcp.connect_ex((host, i)):
+            print(f"Port {i} status: Open")
+        else:
+            print(f"Port {i} status: Closed")
 
-host = input("Host IP you want scan >> ")
-port = int(input("Port from the Host you want scan >> "))
+def main():
+    on = True
+    print(" Py PORT SCANNER ")
+    while on != False:
 
-scanPort(port)
+        host = input("Host IP you want scan >> ")
+        scanPort(host)
+
+        exit_app = input("Exit (y/n) >> ")
+        if exit_app == 'y':
+            on = False
+        else:
+            continue
+
+main()
